7f05109f7c75252464f44bfdd330386bdaa3acf0
cysk003 <cysk003@126.com>
2022-04-13 16:16:21 +0800

    Update duck.sh

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/with-contenv bash
 
 . /app/duck.conf
-RESPONSE=$(curl -sSk "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=")
-if [ "${RESPONSE}" = "OK" ]; then
-    echo "Your IP was updated at $(date)"
-else
-    echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
-fi
+# USERNAME=USERNAME
+# PASSWORD=PASSWORD
+# HOSTNAME=HOSTNAME
+URL_DETECT_IPV4=https://api.ipify.org
+URL_DETECT_IPV6=https://api6.ipify.org
+IPV4ADDR=$(curl -s ${URL_DETECT_IPV4})
+IPV6ADDR=$(curl -s ${URL_DETECT_IPV6})
+AUTH=$(echo "$USERNAME:$PASSWORD" | base64)
+AGENT="$(uname -srom) $USERNAME [Script at https://github.com/axelrindle/noip-update-script]"
+RESPONSE=$(curl --silent --header "Authorization: Basic $AUTH" --header "User-Agent: $AGENT" "https://dynupdate.no-ip.com/nic/update?hostname=$HOSTNAME&myip=$IPV4ADDR&myipv6=$IPV6ADDR")
+echo -e "$(date)\n${RESPONSE}"
