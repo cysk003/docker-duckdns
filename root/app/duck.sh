6f0e452b986915c583e7f714e4c5a8311e8102ea
cysk003 <cysk003@126.com>
2022-04-13 17:25:20 +0800

    Update duck.sh

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/with-contenv bash
 
 . /app/duck.conf
-IPV4ADDR=$(curl -s https://api.ipify.org)
+# SUBDOMAINS=SUBDOMAINS
+# TOKEN=TOKEN
+URL_DETECT_IPV4=https://api.ipify.org
+URL_DETECT_IPV6=https://api6.ipify.org
+IPV4ADDR=$(curl -s ${URL_DETECT_IPV4})
+IPV6ADDR=$(curl -s ${URL_DETECT_IPV6})
 RESPONSE=$(curl -sSk "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4ADDR}")
 if [ "${RESPONSE}" = "OK" ]; then
     echo "Your IP ${IPV4ADDR} was updated at $(date)"
