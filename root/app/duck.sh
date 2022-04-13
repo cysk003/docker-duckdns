7c953f842588cc1c7bb8c777f4417cd41ecebb59
cysk003 <cysk003@126.com>
2022-04-13 16:51:06 +0800

    Update duck.sh

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/with-contenv bash
 
 . /app/duck.conf
-RESPONSE=$(curl -sSk "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=")
-if [ "${RESPONSE}" = "OK" ]; then
-    echo "Your IP was updated at $(date)"
-else
-    echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
-fi
+# USERNAME=USERNAME
+# PASSWORD=PASSWORD
+# HOSTNAME=HOSTNAME
+URL_DETECT_IPV4=https://api.ipify.org
+URL_DETECT_IPV6=https://api6.ipify.org
+IPV4ADDR=$(curl -s ${URL_DETECT_IPV4})
+IPV6ADDR=$(curl -s ${URL_DETECT_IPV6})
+PASSWORDMD5=$(echo "$PASSWORD" | base64)
+RESPONSE=$(curl --silent "https://api.dynu.com/nic/update?username=$USERNAME&password=$PASSWORDMD5&hostname=$HOSTNAME&myip=$IPV4ADDR&myipv6=$IPV6ADDR")
+echo -e "$(date) ${RESPONSE}"
