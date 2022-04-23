a3565e4706facbbf847d3be5690b1753a88a0e22
cysk003 <cysk003@126.com>
2022-04-23 10:53:51 +0800

    Update duck.sh

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -9,7 +9,7 @@ IPV4ADDR=$(curl -s ${URL_DETECT_IPV4})
 IPV6ADDR=$(curl -s ${URL_DETECT_IPV6})
 RESPONSE=$(curl -sSk "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${IPV4ADDR}")
 if [ "${RESPONSE}" = "OK" ]; then
-    echo "Your IP ${IPV4ADDR} was updated at $(date)"
+    echo "Your IP ${SUBDOMAINS} ${IPV4ADDR} was updated at $(date)"
 else
     echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
 fi
