54d55613a04be184481ad29b5c666bab1addd82c
cysk003 <cysk003@126.com>
2022-04-23 10:40:51 +0800

    Update duck.sh

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -4,11 +4,12 @@
 # USERNAME=USERNAME
 # PASSWORD=PASSWORD
 # HOSTNAME=HOSTNAME
+# LOCATION=LOCATION
 # ALIAS=ALIAS
 URL_DETECT_IPV4=https://api.ipify.org
 URL_DETECT_IPV6=https://api6.ipify.org
 IPV4ADDR=$(curl -s ${URL_DETECT_IPV4})
 IPV6ADDR=$(curl -s ${URL_DETECT_IPV6})
 PASSWORDMD5=$(echo "$PASSWORD" | base64)
-RESPONSE=$(curl --silent "https://api.dynu.com/nic/update?username=$USERNAME&password=$PASSWORDMD5&hostname=$HOSTNAME&alias=$ALIAS&myip=$IPV4ADDR&myipv6=$IPV6ADDR")
-echo -e "$(date) ${RESPONSE} $IPV4ADDR"
+RESPONSE=$(curl --silent "https://api.dynu.com/nic/update?username=$USERNAME&password=$PASSWORDMD5&hostname=$HOSTNAME&location=$LOCATION&alias=$ALIAS&myip=$IPV4ADDR&myipv6=$IPV6ADDR")
+echo -e "$(date) $HOSTNAME $IPV4ADDR ${RESPONSE} "
